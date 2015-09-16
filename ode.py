778f732f6a3ce81149f08f338aab243fe0d71f6d
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2015-09-16 22:04:53 +0900

    add forward euler method

--- ode.py
+++ ode.py
@@ -8,6 +8,15 @@ Created on Wed Sep 16 21:39:30 2015
 import matplotlib.pyplot as plt
 import numpy as np
 
+def calc_euler(x0, y0, dt, n , f):
+    x = [x0]
+    y = [y0]
+    for i in range(n):
+        y.append(y[-1] + dt * f(x[-1], y[-1]))
+        x.append(x[-1] + dt)
+        
+    return x, y
+
 
 def func(x, y):
     return x + y
@@ -18,22 +27,25 @@ def main():
     y0 = 1.0
     n = 109
     y = {}
+
+    x, y['euler'] = calc_euler(x0, y0, h, n, func)
     
 
     for i in range(0, n, 10):
         print '%10.4f :' % x[i] ,
         for tmp_y in y.values():
             print ' %10.4f' % tmp_y[i] ,
-        #print '%10.4f : %10.4f %10.4f %10.4f' % (x[i], y['euler'][i], y['heun'][i], y['rk4'][i])
         print ''
 
 
 
-    linestyle = ['-', ':', '--']
+    linestyle = ['-', ':', '--', '-.']
+    for k,v in y.items():
+        plt.plot(x, y[k], linestyle=(linestyle[i % len(linestyle)]), linewidth=2, label=k)
 
-    for i, tmp_y in enumerate(y.values()):
-        plt.plot(x, tmp_y)
-        #plt.plot(x, tmp_y, linestyle=(linestyle[i % len(linestyle)]))
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend(loc=2)
     plt.show()
 
 if __name__ == '__main__':
